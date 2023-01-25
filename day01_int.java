920c2a50f300c7a0eaaa5906e5fe1f5a5ed8f5cf
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-25 15:53:43 +0900

    Add files via upload

---
+++ day01_int.java
@@ -0,0 +1,15 @@
+
+public class day01_int {
+
+	public static void main(String[] args) {
+		
+		int	s = 15 / 2;  // 몫의 정수값 7을 반환
+		int p = 13 % 4;  // 나머지 1 반환
+		System.out.printf("s = %d, p = %d\n", s,p);
+
+		int m = 3;
+		m++;
+		System.out.printf("m = %d \n", m);
+	}
+
+}
